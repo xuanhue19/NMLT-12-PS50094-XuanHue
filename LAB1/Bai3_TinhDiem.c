e4383370b829373cc283f7030f8a857f6b62b0f8
xuanhue19 <xuanhue29@gmail.com>
2026-07-08 18:42:31 +0700

    hoan thanh bai 3 lab 1

--- LAB1/Bai3_TinhDiem.c
+++ LAB1/Bai3_TinhDiem.c
@@ -4,21 +4,22 @@ int main() {
     char hoten[100];
     float toan, ly, hoa;
     float diemtb;
-    printf("Nhap MSSV; ");
+    printf("Nhap MSSV: ");
     scanf("%s", mssv);
-    printf("Nhap ho ten; ");
+    printf("Nhap ho ten: ");
     scanf(" %[^\n]", hoten);   
-    printf("Nhap diem toan; ");
+    printf("Nhap diem toan: ");
     scanf("%f", &toan);
-    printf("Nhap diem Ly; ");
+    printf("Nhap diem Ly: ");
     scanf("%f", &ly);
-    printf("Nhap diem Hoa; ");
+    printf("Nhap diem Hoa: ");
     scanf("%f", &hoa);
     diemtb = (toan * 2 + ly + hoa) / 4;
     printf("\n");
     printf("MSSV: %s\n", mssv);
     printf("Ho ten: %s\n", hoten);
     printf("Diem trung binh: %.2f\n", diemtb);
+
 
     return 0;
 }
