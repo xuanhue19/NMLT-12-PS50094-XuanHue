c33c911ac54bd534ceca777f78ba7c30d9f9b9eb
xuanhue19 <xuanhue29@gmail.com>
2026-07-29 19:50:08 +0700

    Hoan thanh LAB 4

--- LAB4/Main_menulab4.c
+++ LAB4/Main_menulab4.c
@@ -17,38 +17,146 @@ int main()
         scanf("%d", &chon);
 
         switch (chon)
+        {
+        case 1:
+        {
+            int min, max;
+            int i;
+            int tong = 0;
+            int Biendem = 0;
+            float Trungbinh;
+
+            printf("Ban chon chuc nang 1 \n");
+
+            printf("Nhap vao so nguyen min: \n");
+            scanf("%d", &min);
+
+            printf("Nhap vao so nguyen max: \n");
+            scanf("%d", &max);
+
+            if (min > max)
             {
-            case 1:
-            {
-                printf("Ban chon chuc nang 1 \n");
+                printf("Loi: min khong duoc lon hon max! \n");
                 break;
             }
 
-            case 2:
+            else
             {
-                printf("Ban chon chuc nang 2 \n");
+                printf("Cac so chia het cho 2 trong khoảng [%d,%d] la: ", min, max);
+            
+                for (i = min; i <= max; i++)
+
+                {
+                    if (i % 2 == 0)
+                    {
+                        printf ("%d ", i);
+                        tong += i;
+                        Biendem++;
+                    }
+                }
+
+                if (Biendem == 0)
+                {
+                    printf("Khong co so nao chia het cho 2 \n");
+                }
+
+                else
+                {
+                    Trungbinh = (float) tong / Biendem;
+
+                    printf("tong = %d\n", tong);
+                    printf("Biendem = %d\n", Biendem);
+                    printf("Trungbinh = %.2f\n", Trungbinh);
+                }
+            }
+            break;
+        }
+
+        case 2:
+        {
+            int x;
+            int i;
+            int laSonguyento = 1;
+
+            printf("Ban chon chuc nang 2 \n");
+
+            printf("Nhap x: ");
+            scanf("%d", &x);
+
+            if (x < 2)
+            {
+                printf("x khong phai la so nguyen to. \n");
                 break;
             }
 
-            case 3:
+                for (i = 2; i < x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        laSonguyento = 0;
+                        break;
+                    }
+                }
+
+                if (laSonguyento == 1)
+                {
+                    printf("x la so nguyen to. \n");
+                }
+
+                else
+                {
+                    printf("x khong phai la so nguyen to. \n");
+                }
+            
+            break;
+        }
+
+        case 3:
+        {
+            int x;
+            int i;
+            int laSochinhphuong = 0;
+
+            printf("Ban chon chuc nang 3 \n");
+
+            printf("Nhap vao so nguyen x: \n");
+            scanf("%d", &x);
+
+            for (i = 0; i <= x; i++)
             {
-                printf("Ban chon chuc nang 3 \n");
-                break;
+
+                if (i * i == x)
+                {
+                    laSochinhphuong = 1;
+                    break;
+                }
             }
 
-            case 4:
-            {
-                printf("Da thoat ung dung \n");
-                break;
-            }
+                if (laSochinhphuong == 1)
+                {
+                    printf("x la so chinh phuong. \n");
+                }
 
-            default:
-            {
-                printf ("Lua chon khong hop le! Vui long chon lai \n");
-                break;
-            }
-            }
+                else
+                {
+                    printf("x khong phai la so chinh phuong. \n");
+                }
 
+            break;
+        }
+
+        case 4:
+        {
+            printf("Da thoat ung dung \n");
+            break;
+        }
+
+        default:
+        {
+            printf("Lua chon khong hop le! Vui long chon lai \n");
+            break;
+        }
+        }
 
     } while (chon != 4);
 
